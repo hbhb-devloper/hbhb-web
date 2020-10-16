fb58a8c2b298f9f1bf3060db0b63bc89ce9d4489
xiaokang <13148807065@163.com>
2020-10-16 09:29:26 +0800

    调整

---
+++ src/main/java/com/hbhb/web/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.hbhb.web.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+/**
+ * @author xiaokang
+ * @since 2020-09-24
+ */
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource(Environment env) {
+        HikariDataSource ds = new HikariDataSource();
+        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        ds.setJdbcUrl(env.getProperty("spring.datasource.url"));
+        ds.setUsername(env.getProperty("spring.datasource.username"));
+        ds.setPassword(env.getProperty("spring.datasource.password"));
+        return ds;
+    }
+}
