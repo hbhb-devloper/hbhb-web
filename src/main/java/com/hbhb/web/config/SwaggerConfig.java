89dbd87df3f1cd02f60ee10c72f37a51a6f230ac
xiaokang <13148807065@163.com>
2020-10-22 17:05:09 +0800

    调整

--- src/main/java/com/hbhb/web/config/SwaggerConfig.java
+++ src/main/java/com/hbhb/web/config/SwaggerConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 /**
@@ -28,10 +26,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"))
-                        .addHeaders("Content-Security-Policy", new Header().schema(new StringSchema()._default("upgrade-insecure-requests")))
-                )
+                .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
                         .title(title)
                         .version(version)
