7826144ccc1be366e3638aded27ef483eaae7698
xiaokang <13148807065@163.com>
2020-10-23 00:29:31 +0800

    调整

--- src/main/java/com/hbhb/web/config/SwaggerConfig.java
+++ src/main/java/com/hbhb/web/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.hbhb.web.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -25,9 +26,14 @@ public class SwaggerConfig {
     private String description;
     @Value("${springdoc.server-url}")
     private String defaultServerUrl;
+    @Value("${spring.profiles.active}")
+    private String profile;
 
     @Bean
     public OpenAPI customOpenApi() {
+        if (!StringUtils.isEmpty(profile) && !"prd".equals(profile)) {
+            defaultServerUrl += profile;
+        }
         return new OpenAPI()
                 .addServersItem(new Server().url(defaultServerUrl))
                 .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
