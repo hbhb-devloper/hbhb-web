274a9eda0a5b97527e5c7108b22c4b5d9321aaf2
xiaokang <13148807065@163.com>
2020-10-23 02:53:01 +0800

    调整

--- src/main/java/com/hbhb/web/config/SwaggerConfig.java
+++ src/main/java/com/hbhb/web/config/SwaggerConfig.java
@@ -1,13 +1,18 @@
 package com.hbhb.web.config;
 
+import com.hbhb.core.constants.AuthConstant;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
+import java.util.Collections;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
@@ -36,7 +41,12 @@ public class SwaggerConfig {
         }
         return new OpenAPI()
                 .addServersItem(new Server().url(defaultServerUrl))
-                .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .components(new Components().addSecuritySchemes(AuthConstant.JWT_TOKEN_HEADER.value(),
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name(AuthConstant.JWT_TOKEN_HEADER.value())))
+                .security(Collections.singletonList(new SecurityRequirement().addList(AuthConstant.JWT_TOKEN_HEADER.value())))
                 .info(new Info()
                         .title(title)
                         .version(version)
